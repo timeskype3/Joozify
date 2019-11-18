d42d8e84c0d997f774c59088ce57819306479033
aicquewk <vuttithatkrongyot@gmail.com>
2019-11-19 00:39:19 +0700

    edit package-lock.json

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Login from './component/LoginForm';
+import Login from './component/LoginForm.js';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<Login />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
