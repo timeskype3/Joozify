ce4ece0b573884016f07a71c6e28bee5ff526dc9
aicquewk <vuttithatkrongyot@gmail.com>
2019-11-21 22:36:24 +0700

    Edit login field

--- FrontEnd/src/component/LoginForm.js
+++ FrontEnd/src/component/LoginForm.js
@@ -35,7 +35,6 @@ class LoginForm extends React.Component {
     e.preventDefault();
 
     const { email, password } = this.state;
-    // TODO: implement signInWithEmailAndPassword()
     auth
       .signInWithEmailAndPassword(email, password)
       .then(response => {
@@ -71,45 +70,36 @@ class LoginForm extends React.Component {
       );
     }
     return (
-      <section className="section container">
-        <div className="columns is-centered">
-          <div className="column is-half">
-            <form onSubmit={this.onSubmit}>
-              <div className="field">
-                <label className="label"> Email </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="email"
-                    name="email"
-                    onChange={this.onChange}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label"> Password </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="password"
-                    name="password"
-                    onChange={this.onChange}
-                  />
-                </div>
-              </div>
-              {message ? <p className="help is-danger"> {message} </p> : null}
-              <div className="field is-grouped">
-                <div className="control">
-                  <button className="button is-link"> Submit </button>
-                </div>
-                <div className="control">
-                  <button className="button is-text"> Cancel </button>
-                </div>
-              </div>
-            </form>
+      <form onSubmit={this.onSubmit}>
+        <div className="field">
+          <label className="label"> Email </label>
+          <div className="control">
+            <input
+              className="input"
+              type="email"
+              name="email"
+              onChange={this.onChange}
+            />
           </div>
         </div>
-      </section>
+        <div className="field">
+          <label className="label"> Password </label>
+          <div className="control">
+            <input
+              className="input"
+              type="password"
+              name="password"
+              onChange={this.onChange}
+            />
+          </div>
+        </div>
+        {message ? <p className="help is-danger"> {message} </p> : null}
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link"> Login </button>
+          </div>
+        </div>
+      </form>
     );
   }
 }
