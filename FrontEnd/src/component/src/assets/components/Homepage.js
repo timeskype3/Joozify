0905d34d2f4508dbccb704b2742c59893df0683a
Jirayu Janraluek <mewti@hotmail.com>
2019-11-24 23:32:27 +0700

    Scroll Main (2)

--- FrontEnd/src/component/src/assets/components/Homepage.js
+++ FrontEnd/src/component/src/assets/components/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import MovieCarousel from './Carousel';
 import Sections from './sections/Sections';
+
 class Homepage extends React.Component {
   render() {
     return (
