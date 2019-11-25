cbf769802506886dcf8d8e2d30915cafc23d6548
aicquewk <vuttithatkrongyot@gmail.com>
2019-11-26 01:06:17 +0700

    Edit <h> tag

--- FrontEnd/src/component/Login.js
+++ FrontEnd/src/component/Login.js
@@ -59,9 +59,9 @@ export default class Login extends Component {
     return (
       <div className="Login">
         <div className="Titile">
-          <h className="Logo">Jooz</h>
-          <h className="Logo-2">i</h>
-          <h className="Logo">fy. </h>
+          <h1 className="Logo">Jooz</h1>
+          <h1 className="Logo-2">i</h1>
+          <h1 className="Logo">fy. </h1>
           <h2>A millions songs </h2>
           <h3>that on your hand</h3>
           <Link style={navStyle} to="/main">
